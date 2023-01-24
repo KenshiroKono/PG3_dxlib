75838dccbdecea21738ba4b8f1f3a5618d262fe6
KenshiroKono <k021g1265@g.neec.ac.jp>
2023-01-24 15:41:40 +0900

    プロジェクト ファイルを追加します。

---
+++ SceneManager.cpp
@@ -0,0 +1,20 @@
+#include "SceneManager.h"
+SceneManager::SceneManager() {
+}
+SceneManager::~SceneManager() {
+}
+
+SceneManager* SceneManager::GetInstance() {
+	static SceneManager instance;
+
+	return &instance;
+}
+
+void SceneManager::SceneChange(int& sceneNo) {
+	if (sceneNo < 3) {
+		sceneNo += 1;
+	}
+	else {
+		sceneNo = 0;
+	}
+}
